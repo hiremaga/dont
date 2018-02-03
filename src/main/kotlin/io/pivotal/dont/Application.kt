945daf039cce4b1f32242bc0df9c4b0cb0e028ca
Abhi Hiremagalur <ahiremagalur@pivotal.io>
2018-02-03 14:39:21 -0800

    Remove redudant prefix from Spring Boot Application

--- src/main/kotlin/io/pivotal/dont/Application.kt
+++ src/main/kotlin/io/pivotal/dont/Application.kt
@@ -4,8 +4,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 
 @SpringBootApplication
-class DontApplication
+class Application
 
 fun main(args: Array<String>) {
-    runApplication<DontApplication>(*args)
+    runApplication<Application>(*args)
 }
--- src/test/kotlin/io/pivotal/dont/ApplicationTests.kt
+++ src/test/kotlin/io/pivotal/dont/ApplicationTests.kt
@@ -7,7 +7,7 @@ import org.springframework.test.context.junit4.SpringRunner
 
 @RunWith(SpringRunner::class)
 @SpringBootTest
-class DontApplicationTests {
+class ApplicationTests {
 
 	@Test
 	fun contextLoads() {
