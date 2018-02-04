7f496762f2f06618ea18e8dbb91934742ec47f0d
Abhi Hiremagalur <ahiremagalur@pivotal.io>
2018-02-04 08:33:37 -0800

    Backfill simple test for API endpoint
    Adds restassured, shows how to use with MockMVC to test controllers

---
+++ api/src/test/kotlin/io/pivotal/dont/ApiTest.kt
@@ -0,0 +1,27 @@
+package io.pivotal.dont
+
+import org.junit.Test
+import org.springframework.boot.test.context.SpringBootTest
+import org.hamcrest.Matchers.*
+
+import io.restassured.module.mockmvc.RestAssuredMockMvc.*
+import org.junit.runner.RunWith
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.test.context.junit4.SpringRunner
+
+@RunWith(SpringRunner::class)
+@SpringBootTest
+class ApiTest {
+    @Autowired
+    lateinit var api: Api
+
+    @Test
+    fun `it responds with a simple example`() {
+        given()
+            .standaloneSetup(api)
+        .`when`()
+            .get("/")
+        .then()
+                .body(equalTo("hello there!"))
+    }
+}
