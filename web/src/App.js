50ed121937dce904bf52e7b28a9976c46f9ee505
Abhi Hiremagalur <ahiremagalur@pivotal.io>
2018-02-04 11:55:34 -0800

    Incorporate example from Pivotal UI

--- web/src/App.js
+++ web/src/App.js
@@ -1,21 +1,29 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
+import React, {Component} from 'react';
 import './App.css';
+import {DefaultButton} from 'pivotal-ui/react/buttons';
+import {Input} from 'pivotal-ui/react/inputs';
+import {BaseModal, ModalBody, ModalFooter} from 'pivotal-ui/react/modals';
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
-  }
+    constructor(props) {
+        super(props);
+        this.state = {modalOpen: false};
+    }
+
+    render() {
+        return (
+            <div>
+                <DefaultButton onClick={() => this.setState({modalOpen: true})}> Open Stateless Modal </DefaultButton>
+                <BaseModal acquireFocus={false} title='What a Header!' className='optional-custom-class'
+                           show={this.state.modalOpen} onHide={() => this.setState({modalOpen: false})}> <ModalBody>
+                    <p>Text in a body</p><Input autoFocus placeholder="Tell me your darkest secrets"/></ModalBody>
+                    <ModalFooter>
+                        <DefaultButton onClick={() => this.setState({modalOpen: false})}> Close </DefaultButton>
+                    </ModalFooter>
+                </BaseModal>
+            </div>
+        );
+    }
 }
 
 export default App;
